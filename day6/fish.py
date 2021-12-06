8a79c6e7706b773a466d115ecb2bdb4a7bb39b63
Elly <elly.shao.94@gmail.com>
2021-12-06 00:11:43 -0800

    part 2 and some cleanup

--- day6/fish.py
+++ day6/fish.py
@@ -6,27 +6,31 @@ from collections import Counter
 puzzinput = open("input.txt")
 line = puzzinput.readline()
 
+# each incidence of a number in the line
+# is a fish with that internal timer value
 fish = dict(Counter(map(int, line.split(','))))
 # fill in the rest of the dictionary just in case
 for i in range(9):
 	if i not in fish:
 		fish[i] = 0
 
+# demo
 # numdays = 18
-numdays = 80
+# Part 1: 80 days
+# numdays = 80
+# Part 2: 256 days
+numdays = 256
 
 for i in range(numdays):
 	# when the fish counter reaches 0,
 	# spawn a new fish and reset counter to 6
 	spawns = fish[0]
-	fish[0] = fish[1]
-	fish[1] = fish[2]
-	fish[2] = fish[3]
-	fish[3] = fish[4]
-	fish[4] = fish[5]
-	fish[5] = fish[6]
-	fish[6] = fish[7] + spawns # the reset fish
-	fish[7] = fish[8]
+
+	# bump all the fish 1 day
+	for t in range(8):
+		fish[t] = fish[t+1]
+
+	fish[6] += spawns # the reset fish
 	fish[8] = spawns # the new fish
 
 totalfish = 0
