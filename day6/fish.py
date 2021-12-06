0524b93460533675184ceffc84441f7386195d53
Elly <elly.shao.94@gmail.com>
2021-12-06 00:30:02 -0800

    list instead of dict

--- day6/fish.py
+++ day6/fish.py
@@ -8,11 +8,9 @@ line = puzzinput.readline()
 
 # each incidence of a number in the line
 # is a fish with that internal timer value
-fish = dict(Counter(map(int, line.split(','))))
-# fill in the rest of the dictionary just in case
-for i in range(9):
-	if i not in fish:
-		fish[i] = 0
+inputfish = list(map(int, line.split(',')))
+# index is the timer num, value is # fish with that timer
+fish = [inputfish.count(i) for i in range(9)]
 
 # demo
 # numdays = 18
@@ -21,20 +19,16 @@ for i in range(9):
 # Part 2: 256 days
 numdays = 256
 
+# for each day, decrement all the fish timers
+# if they hit 0, spawn a new fish w/ timer 8, and reset timer to 6
 for i in range(numdays):
-	# when the fish counter reaches 0,
-	# spawn a new fish and reset counter to 6
-	spawns = fish[0]
+	# left rotate all the fish by 1
+	# the old fish[0] is now fish[8]
+	fish = fish[1:] + fish[:1]
 
-	# bump all the fish 1 day
-	for t in range(8):
-		fish[t] = fish[t+1]
+	#number of reset fish = number of new fish
+	fish[6] += fish[8]
 
-	fish[6] += spawns # the reset fish
-	fish[8] = spawns # the new fish
-
-totalfish = 0
-for numfish in fish.values():
-	totalfish += numfish
+totalfish = sum(fish)
 
 print("Total fish", totalfish)
