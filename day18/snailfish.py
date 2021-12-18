0df7a199061879b4c64f9985ed04883311d5b79e
jellybeane <elly.shao.94@gmail.com>
2021-12-17 23:45:46 -0800

    getting closer

--- day18/snailfish.py
+++ day18/snailfish.py
@@ -1,4 +1,5 @@
 import math
+from collections import deque
 
 class SnailNum():
 
@@ -8,18 +9,26 @@ class SnailNum():
         
         return 1 + self.parent.depth()
 
-    def magnitude(self):
-        return NotImplemented # should be overridden (?)
-
     def __add__(self, other):
-        return SnailPair(self, other)
+        return SnailPair(lst=None, left=self, right=other)
 
 class SnailPair(SnailNum):
-    def __init__(self, left, right):
-        self.left = left
-        self.left.parent = self
+    def __init__(self, lst=None, left=None, right=None):
+        # initializing from list
+        if not lst is None:
+            if isinstance(lst[0], int):
+                self.left = SnailRegular(lst[0])
+            else:
+                self.left = SnailPair(lst[0])
+            if isinstance(lst[1], int):
+                self.right = SnailRegular(lst[1])
+            else:
+                self.right = SnailPair(lst[1])
+        else: # initializing from left/right
+            self.left = left
+            self.right = right
 
-        self.right = right
+        self.left.parent = self
         self.right.parent = self
 
         self.parent = None
@@ -32,11 +41,28 @@ class SnailPair(SnailNum):
         self.right = right
         self.right.parent = self
 
-    def magnitude(self):
-        return 3*self.left.magnitude() + 2*self.right.magnitude()
+    def explode(self, depth):
+        # check if the children need to explode
+        if depth < 4:
+            leftexplode = self.left.explode(depth+1)
+            if leftexplode[0]:
+                if not leftexplode[1] is None:
+                    # the left child needs to be replaced
+                    self.setleft(leftexplode[1])
+                    return True, None
+            rightexplode = self.right.explode(depth+1)
+            if rightexplode[0]:
+                if not rightexplode[1] is None:
+                    # the right child needs to be replaced
+                    self.setright(rightexplode[1])
+                    return True, None
+            # came back, no explodes needed
+            return False, None
 
-    def explode(self):
-        # the left neighbor
+        # otherwise, this is the one to explode
+
+        # TODO bug in the neighbor-finding, see smalldemo
+        # find the left neighbor
         curr = self
         # go up until you can go left
         while not curr.parent is None and curr == curr.parent.left:
@@ -69,17 +95,83 @@ class SnailPair(SnailNum):
             curr.val += self.right.val
 
         # replacement for self
-        return SnailNum(0)
+        return True, SnailRegular(0)
+
+    def split(self):
+        leftsplit = self.left.split()
+        if leftsplit[0]: # did a split happend down the left branch?
+            if not leftsplit[1] is None:
+                # the left child needs to be replaced
+                self.setleft(leftsplit[1])
+                return True, None
+            return leftsplit
+        rightsplit = self.right.split()
+        if rightsplit[0]: # did a split happend down the right branch?
+            if not rightsplit[1] is None:
+                # the right child needs to be replaced
+                self.setright(rightsplit[1])
+                return True, None
+            return rightsplit
+        # no splits needed
+        return False, None
+    
+    def reduce(self):
+        while True:
+            exploded = self.explode(self.depth())
+            if not exploded[0]:
+                splitted = self.split()
+                if not splitted[0]:
+                    # if nothing exploded or split, we're done
+                    break
+    
+    def magnitude(self):
+        return 3*self.left.magnitude() + 2*self.right.magnitude()
+
+    def __str__(self):
+        return '['+ str(self.left) + ',' + str(self.right) + ']'
 
 class SnailRegular(SnailNum):
     def __init__(self, val):
         self.val = val
         self.parent = None
 
+    def split(self):
+        if self.val < 10:
+            return False, None
+
+        leftval = SnailRegular(int(math.floor(self.val/2)))
+        rightval = SnailRegular(int(math.ceil(self.val/2)))
+        return True, SnailPair(lst=None, left=leftval,right=rightval)
+    
+    def explode(self, depth):
+        return False, None
+
     def magnitude(self):
         return self.val
 
-    def split(self):
-        leftval = int(math.floor(self.val/2))
-        rightval = int(math.ceil(self.val/2))
-        return SnailPair(leftval,rightval)
+    def __str__(self):
+        return str(self.val)
+
+# s1 = SnailPair([[[[4,3],4],4],[7,[[8,4],9]]])
+# s2 = SnailPair([1,1])
+# s = s1 + s2
+# s.reduce() 
+# print(str(s))
+
+puzzinput = open("smalldemo.txt")
+#puzzinput = open("demo.txt")
+#puzzinput = open("input.txt")
+
+snails = None
+for line in puzzinput:
+    s = SnailPair(lst=eval(line.strip()))
+    print("New snail", s)
+    if snails is None:
+        snails = s
+    else:
+        snails += s
+    
+    snails.reduce()
+    print("Sum", snails)
+
+print(snails.magnitude())
