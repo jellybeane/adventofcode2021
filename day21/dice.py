67e3cd611e8d253ef0bd16d40b221b3da941e75a
jellybeane <elly.shao.94@gmail.com>
2021-12-21 20:03:53 -0800

    zero is falsy

--- day21/dice.py
+++ day21/dice.py
@@ -1,20 +1,35 @@
-#pos = [3,7] given positions are 1-indexed
-pos = [0,9]
+import math
+from collections import Counter
+
+#pos = [4,8] #given positions are 1-indexed
+pos = [1,10]
 score = [0,0]
 
 roll = 0
 while score[0] < 1000 and score[1] < 1000:
     index = roll % 2
-    sum = 0
     destination = pos[index]
     # each player rolls 3 times
     for i in range(3):
         roll += 1
-        destination = (destination + roll) % 10
+        destination = (destination + roll) % 10 or 10
     pos[index] = destination
-    # the board is 1-indexed
-    score[index] += destination+1
-    print("player %s: %s %s" % (2-index, destination+1, score[index]))
+    score[index] += destination
+    print("player %s: %s %s" % (2-index, destination, score[index]))
 
 # part 1: losing score times number of rolls
 print(roll * min(score[0], score[1]))
+
+ # part 2: ???
+
+# map roll total to # ways 3 rolls can sum to it
+testrolls = []
+for i in range(1,4):
+    for j in range(1,4):
+        for k in range(1,4):
+            testrolls.append(i+j+k)
+waystoroll = Counter(testrolls)
+
+threshold = 2
+#for turn in range(threshold*2):
+
